96792500b2ca96df9daf3252f7654ada9c25d0a7
maqeelabbas <107469006+maqeelabbas@users.noreply.github.com>
2025-11-15 15:34:01 +0100

    fixed CI build errors

--- apps/public-web/src/app/app.spec.ts
+++ apps/public-web/src/app/app.spec.ts
@@ -1,20 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should have a title', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app.title).toBe('public-web');
+  });
+
+  it('should render router outlet', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome public-web'
-    );
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
